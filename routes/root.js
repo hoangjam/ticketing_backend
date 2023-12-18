11c3c61ed761aee63aeed85452060c9c51ceebde
hoangjam <j.hoang2080@gmail.com>
2023-12-18 11:42:58 -0500

    add comments

--- routes/root.js
+++ routes/root.js
@@ -1,9 +1,17 @@
+// import the Express.js module
 const express = require('express')
+
+// create a new router object
 const router = express.Router()
+
+// import the built-in Node.js ‘path’ module
 const path = require('path')
 
+// set up a route handler for GET requests to the root URL '/' or '/index' or 'index.html'
+// when a client makes a GET request to any of these URLs, server sends response index.html
 router.get('^/$|/index(.html)?', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'views', 'index.html'))
 })
 
+// export the router object
 module.exports = router
--- server.js
+++ server.js
@@ -1,10 +1,20 @@
+// import the Express.js module and create an Express.js application
 const express = require('express')
 const app = express()
+
+// import the built-in Node.js ‘path’ module
 const path = require('path')
+
+// set the port on which the server will listen
 const PORT = process.env.PORT || 3500
 
+// tell the server to serve static files (HTML, CSS, and JS files) from the ‘public’ directory when the client navigates to the root URL (/).
 app.use('/', express.static(path.join(__dirname, '/public')))
+
+// import a router object from the ‘root’ module in the ‘routes’ directory and mounts it at the root URL (/)
 app.use('/', require('./routes/root'))
+
+// set up a catch-all route handler that responds with a 404 error when the client navigates to a URL that doesn’t match any of the defined routes
 app.all('*', (req, res) => {
     res.status(404)
     if (req.accepts('html')){
@@ -16,4 +26,5 @@ app.all('*', (req, res) => {
     }
 })
 
+// starts the server and makes it listen for requests on the specified port
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
